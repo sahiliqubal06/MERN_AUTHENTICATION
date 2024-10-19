dddc9b72d5f2739495ccfcd25ff5a6f43f966f24
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-10-19 12:38:40 +0530

    Added auth routes,controller,model,utils and enabled JSON parsing, and updated port configuration

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+import { User } from "../models/user.model";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (errror) {
+    res.status(400).json({ success: false, message: errror.message });
+  }
+  res.send("signup route");
+};
+
+export const login = async (req, res) => {
+  res.send("login route");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout route");
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port:", PORT);
 });
-
-
