f2c35a0ab477ceec2cfdb7a4fb3d73dc027c9322
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-10-22 23:51:31 +0530

    Added password reset functionality to auth controller and routes, including email sending

--- Backend/mailtrap/emails.js
+++ Backend/mailtrap/emails.js
@@ -1,5 +1,6 @@
 import {
   PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
   VERIFICATION_EMAIL_TEMPLATE,
 } from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
@@ -56,5 +57,22 @@ export const sendPasswordResetEmail = async (email, resetURL) => {
   } catch (error) {
     console.log(`Error sending password reset email`, error);
     throw new Error(`Error sending password reset email: ${error}`);
+  }
+};
+
+export const sendResetSuccessEmail = async (email) => {
+  const recipient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Password Reset Successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    });
+    console.log("Password reset email sent successfully", response);
+  } catch (error) {
+    console.log(`Error sending password reset success email`, error);
+    throw new Error(`Error sending password reset success email: ${error}`);
   }
 };
