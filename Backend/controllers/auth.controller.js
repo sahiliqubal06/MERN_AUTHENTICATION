5890411f79cce520b01c76cd0efd1698491440b8
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-10-26 12:13:22 +0530

    Fixed the error in auth controller

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -148,7 +148,7 @@ export const forgotPassword = async (req, res) => {
     const resetToken = crypto.randomBytes(20).toString("hex");
     const resetTokenExpireAt = Date.now() + 1 * 60 * 60 * 1000;
     user.resetPasswordToken = resetToken;
-    user.resetPasswordExpireAt = resetTokenExpireAt;
+    user.resetPasswordExpiresAt = resetTokenExpireAt;
     await user.save();
     await sendPasswordResetEmail(
       user.email,
@@ -171,16 +171,16 @@ export const resetPassword = async (req, res) => {
   try {
     const { token } = req.params;
     const { password } = req.body;
-
     const user = await User.findOne({
       resetPasswordToken: token,
       resetPasswordExpiresAt: { $gt: Date.now() },
     });
 
     if (!user) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Invalid or expired reset token" });
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or expired reset token",
+      });
     }
 
     // update password
@@ -193,12 +193,16 @@ export const resetPassword = async (req, res) => {
 
     await sendResetSuccessEmail(user.email);
 
-    res
-      .status(200)
-      .json({ success: true, message: "Password reset successful" });
+    res.status(200).json({
+      success: true,
+      message: "Password reset successful",
+    });
   } catch (error) {
     console.log("Error in resetPassword ", error);
-    res.status(400).json({ success: false, message: error.message });
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
 
