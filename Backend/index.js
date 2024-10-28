fc55f99fba6c53a2143648f6fd86cfa2895be661
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-10-28 22:06:52 +0530

    Added CORS middleware, updated frontend dependencies, and modified SignUpPage component

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
@@ -8,6 +9,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
